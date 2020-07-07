94704084234a050adacda2e3494d297dbdde13ff
daniel molina <bigdpower@icloud.com>
2020-07-07 09:38:35 -0700

    Updates

---
+++ precache-manifest.f9bcd7f1f1858ac020e4933a31f2e08a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eee1236837db62ac4df4727a43cea60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7212811986f6fdf2bcb",
+    "url": "/static/css/main.7c7b820f.chunk.css"
+  },
+  {
+    "revision": "1da80375654b644304dc",
+    "url": "/static/js/2.cf0241c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf0241c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7212811986f6fdf2bcb",
+    "url": "/static/js/main.d44c93f5.chunk.js"
+  },
+  {
+    "revision": "0239a00348e1c49388c7",
+    "url": "/static/js/runtime-main.898530a7.js"
+  }
+]);
